d1689c290dab50de402e474f99446f66d7894461
Diogo Sobral <dsforasteiro@gmail.com>
2023-09-17 21:59:32 -0300

    Merge

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -12,24 +12,37 @@ public class CameraFollowPlayer : MonoBehaviour {
     [SerializeField] private float _cameraVelocity = 1f;
     [SerializeField] private TMP_Text _timeText;
     [SerializeField] private float _maxHigh = 71f;
+    [SerializeField] private bool _isSinglePlayer;
+    [SerializeField] private float _delayForCamera = 3f;
     private bool _canFollow = true;
     public float _currentMinutes;
     public float _currentSeconds;
 
     // Update is called once per frame
     void Update() {
-        if(_player1.transform.position.y > _maxHigh || _player2.transform.position.y > _maxHigh) _canFollow = false;
-
-        if(_time - Time.time >= 0 && _canFollow) {
-            if (_player1.transform.position.y > _player2.transform.position.y) _vcam.Follow = _player1.transform;
-            else _vcam.Follow = _player2.transform;
-
+        if(_isSinglePlayer) {
             _currentMinutes = Mathf.Floor((_time - Time.time)/60);
             _currentSeconds = Mathf.Floor(_time - Time.time - (_currentMinutes * 60));
             _timeText.SetText("Time: " + _currentMinutes + ":" + _currentSeconds.ToString("00"));
+
+            Debug.Log(Time.time);
+            if(Time.time >= _delayForCamera) {
+                transform.position = new Vector3(transform.position.x, transform.position.y + (_cameraVelocity * Time.deltaTime), transform.position.z);
+            }
         } else {
-            _vcam.Follow = null;
-            transform.position = new Vector3(transform.position.x, transform.position.y + (_cameraVelocity * Time.deltaTime), transform.position.z);
+            if(_player1.transform.position.y > _maxHigh || _player2.transform.position.y > _maxHigh) _canFollow = false;
+
+            if(_time - Time.time >= 0 && _canFollow) {
+                if (_player1.transform.position.y > _player2.transform.position.y) _vcam.Follow = _player1.transform;
+                else _vcam.Follow = _player2.transform;
+
+                _currentMinutes = Mathf.Floor((_time - Time.time)/60);
+                _currentSeconds = Mathf.Floor(_time - Time.time - (_currentMinutes * 60));
+                _timeText.SetText("Time: " + _currentMinutes + ":" + _currentSeconds.ToString("00"));
+            } else {
+                _vcam.Follow = null;
+                transform.position = new Vector3(transform.position.x, transform.position.y + (_cameraVelocity * Time.deltaTime), transform.position.z);
+            }
         }
     }
 }
