1791312a7b314e4863cf75f0ae5b961c75d44aff
SagravArt <felipebastosv@gmail.com>
2023-11-22 00:12:17 -0300

    transições se pa? (medo

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,20 +1,35 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+    [SerializeField] private float transitionTime = 1f;
+
     private void Start() {
         Time.timeScale = 1;
     }
 
     public void Player1()
     {
-        SceneManager.LoadScene("Player1");
+        transition.SetTrigger("SinglePlayer");
+
+        StartCoroutine(LoadLevel("Player1"));
     }
+
+    IEnumerator LoadLevel(string sceneName)
+    {
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(sceneName);
+    }
+
     public void Player2()
     {
-        SceneManager.LoadScene("Player2");
+        transition.SetTrigger("Multiplayer");
+
+        StartCoroutine(LoadLevel("Player2"));
     }
 }
